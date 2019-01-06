5d44c5cca709bd4b61aa899d1e4020211ef68b8e
Michael D. Norman <michael.norman@denormans.com>
2019-01-06 17:37:34 -0600

    [issue-47] added NotIntrospected to Context

--- src/main/kotlin/com/github/pgutkowski/kgraphql/Context.kt
+++ src/main/kotlin/com/github/pgutkowski/kgraphql/Context.kt
@@ -1,8 +1,9 @@
 package com.github.pgutkowski.kgraphql
 
+import com.github.pgutkowski.kgraphql.schema.introspection.NotIntrospected
 import kotlin.reflect.KClass
 
-
+@NotIntrospected
 class Context(private val map: Map<Any, Any>) {
 
     operator fun <T : Any> get(kClass: KClass<T>): T? {
